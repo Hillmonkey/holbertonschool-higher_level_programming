a8c6a3cddfd7d9d97ed3dae8c21de06535089f46
Larry Madeo <lmadeo76@gmail.com>
2017-08-02 01:43:29 +0000

    switched print format style

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,7 +5,7 @@ last_digit = number % 10
 if number < 0:
     last_digit = -last_digit
 
-print("Last digit of {0} is {1} and is ".format(number, last_digit), end='')
+print("Last digit of {:d} is {:d} and is ".format(number, last_digit), end='')
 if last_digit == 0:
     print('0')
 elif last_digit < 6:
