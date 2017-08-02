e7a91b8cc7838632ff0cd83d354106a3022bdb9c
Larry Madeo <lmadeo76@gmail.com>
2017-08-02 03:55:57 +0000

    no more implicit elses

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -2,10 +2,11 @@
 def remove_char_at(str, n):
     if n < 0 or n >= len(str):
         return str
-    if n == 0:
+    elif n == 0:
         return str[1:len(str)]
-    if n == len(str) - 1:
+    elif n == len(str) - 1:
         return str[:len(str) - 1]
-    pre = str[:n]
-    post = str[n+1: len(str)]
-    return pre + post
+    else:
+        pre = str[:n]
+        post = str[n+1: len(str)]
+        return pre + post
