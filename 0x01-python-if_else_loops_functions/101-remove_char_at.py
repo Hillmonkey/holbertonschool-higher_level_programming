eeb9b3bd1aed92986daf6fbfcc5307e56c91e916
Larry Madeo <lmadeo76@gmail.com>
2017-08-02 04:04:27 +0000

    assign slices to newstr first

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -3,10 +3,13 @@ def remove_char_at(str, n):
     if n < 0 or n >= len(str):
         return str
     elif n == 0:
-        return str[1:len(str)]
+        newstr = str[1:]
+        return newstr
     elif n == len(str) - 1:
-        return str[:len(str) - 1]
+        newstr = str[:len(str) - 1]
+        return newstr
     else:
         pre = str[:n]
         post = str[n+1: len(str)]
-        return pre + post
+        newstr = pre + post
+        return newstr
