fdfbc67295d06b6662c57287f49c4ed1e6b78429
Larry Madeo <lmadeo76@gmail.com>
2017-08-02 00:02:39 +0000

    first commit

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+
+def islower(c):
+    '''islower returns a boolean
+    '''
+    if ord(c) >= ord('a') and ord(c) <= ord('z'):
+        return True
+    else:
+        return False
+
+
+def uppercase(str):
+    offset = ord('A') - ord('a')
+    for c in str:
+        if islower(c):
+            newc = chr(ord(c) + offset)
+        else:
+            newc = c
+        print(newc, end='')
+    print('')
