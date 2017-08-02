ce0332f20163903db9d30f0709dbafaeaa6c1ff9
Larry Madeo <lmadeo76@gmail.com>
2017-08-02 03:25:54 +0000

    refactor for 1 print, not 2

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -4,6 +4,7 @@
 offset = ord('A') - ord('a')
 for i in range(ord('z'), ord('a') - 1, -1):
     if i % 2 == 0:
-        print("{:s}".format(chr(i)), end='')
+        c = chr(i)
     else:
-        print("{:s}".format(chr(i + offset)), end='')
+        c = chr(i + offset)
+    print("{:s}".format(c), end='')
