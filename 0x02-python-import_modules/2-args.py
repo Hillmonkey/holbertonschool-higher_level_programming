1aff2dbf155d84e78224a81376fbc6b3bfd60061
Larry Madeo <lmadeo76@gmail.com>
2017-08-03 21:24:38 +0000

    first commit

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+from sys import argv
+
+if __name__ == "__main__":
+    argc = len(argv)
+
+    if argc <= 1:
+        print("0 argument.")
+    elif argc == 2:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(argc - 1))
+
+    for i in range(1, argc):
+        print("{}: {}".format(i, argv[i]))
