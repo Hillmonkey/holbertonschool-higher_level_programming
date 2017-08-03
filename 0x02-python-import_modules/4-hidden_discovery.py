a24008c5b192638ed9c7649911314eccff74b990
Larry Madeo <lmadeo76@gmail.com>
2017-08-03 22:27:49 +0000

    tabs to spaces ...

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-	import hidden_4
-	dud = dir(hidden_4)
-	for s in dud:
-		if s[:2] != '__':
-			print(s)
+    import hidden_4
+    dud = dir(hidden_4)
+    for s in dud:
+        if s[:2] != '__':
+            print(s)
