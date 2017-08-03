3974f08902a8b593bd2e4e330a1e220973dfb10b
Larry Madeo <lmadeo76@gmail.com>
2017-08-03 20:39:19 +0000

    name to __name__

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 add = __import__('add_0').add
 
-if __name__ == "main":
+if __name__ == "__main__":
     a = 1
     b = 2
     print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
