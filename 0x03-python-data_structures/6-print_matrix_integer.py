2bb00684a01a318acf8c15a18dc5975aaf32abf7
Larry Madeo <lmadeo76@gmail.com>
2017-08-08 23:32:14 +0000

    deal with empty matrix passed

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,6 +4,8 @@
 def print_matrix_integer(matrix=[[]]):
     ''' print a matrix aka 2d array of integers
     '''
+	if len(matrix) == 1 and len(matrix[0] = 0):
+        print()
     if matrix is not None and matrix[0] is not None:
         for inner in matrix:
             for i, n in enumerate(inner):
