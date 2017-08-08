83df8fa699213f5dce6d6c1553755143886e6c3a
Larry Madeo <lmadeo76@gmail.com>
2017-08-08 23:35:21 +0000

    get rid of assignment in conditional

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,7 +4,7 @@
 def print_matrix_integer(matrix=[[]]):
     ''' print a matrix aka 2d array of integers
     '''
-    if len(matrix) == 1 and len(matrix[0] = 0):
+    if len(matrix) == 1 and len(matrix[0] == 0):
         print()
     elif matrix is not None and matrix[0] is not None:
         for inner in matrix:
