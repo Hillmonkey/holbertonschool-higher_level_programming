937291bd70a4cc4c030e532cd1f9630b8acfa164
Larry Madeo <lmadeo76@gmail.com>
2017-08-09 23:25:59 +0000

    more pythonicity

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -5,9 +5,7 @@ def new_in_list(my_list, idx, element):
     ''' replace element in list
     '''
 
-    new = []
-    for el in my_list:
-        new.append(el)
+    new = my_list[:]
     if idx in range(len(new)):
         new[idx] = element
     return new
