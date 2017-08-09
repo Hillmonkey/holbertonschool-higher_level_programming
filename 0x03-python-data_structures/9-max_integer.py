9ae32e2834c80b186d98a2474599e94cc69e21ee
Larry Madeo <lmadeo76@gmail.com>
2017-08-09 02:45:37 +0000

    first commit'

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def max_integer(my_list=[]):
+    ''' returns max integer
+    '''
+
+    if len(my_list) == []:
+        return None
+    max = my_list[0]
+    for i in range(len(my_list)):
+        max = my_list[i] if my_list[i] > max else max
+    return max
