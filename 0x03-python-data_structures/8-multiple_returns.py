bbab7e4f9bea75ab32c5022e33440d709ccbc7e5
Larry Madeo <lmadeo76@gmail.com>
2017-08-09 02:30:27 +0000

    first commit

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+def multiple_returns(sentence):
+    ''' returns tuple:
+    first el: length of sentence
+    second el: first char of sentence
+    '''
+
+    if len(sentence) == 0:
+        first_char is None
+    else:
+        first_char = sentence[0]
+
+    return (len(sentence), first_char)
