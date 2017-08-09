ceb6595102a960d260aa87e8f067da47ce1736e1
Larry Madeo <lmadeo76@gmail.com>
2017-08-09 04:45:38 +0000

    no longer testing len == []

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -5,9 +5,9 @@ def max_integer(my_list=[]):
     ''' returns max integer
     '''
 
-    if my_list is None or len(my_list) == []:
+    if len(my_list) == 0:
         return None
     max = my_list[0]
     for i in range(len(my_list)):
-        max = my_list[i] if my_list[i] > max else max
+        max = my_list[i] if my_list[i] > max
     return max
