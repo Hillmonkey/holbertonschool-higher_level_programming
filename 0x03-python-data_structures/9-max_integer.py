0226c622aeb8ed1004cd05ff7883d511fdc078ea
Larry Madeo <lmadeo76@gmail.com>
2017-08-09 04:47:34 +0000

    recompleted full if else one-liner

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -9,5 +9,5 @@ def max_integer(my_list=[]):
         return None
     max = my_list[0]
     for i in range(len(my_list)):
-        max = my_list[i] if my_list[i] > max
+        max = my_list[i] if my_list[i] > max else max
     return max
