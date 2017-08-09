88736c5208b1c838de0b94ebed6798d74f9c14d7
Larry Madeo <lmadeo76@gmail.com>
2017-08-09 23:10:43 +0000

    more pythonicity

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -4,17 +4,6 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     ''' returns tuple with two sums
     '''
-    new_list = [0, 0]
-    if len(tuple_a) == 0:
-        tuple_a = (0, 0)
-    if len(tuple_b) == 0:
-        tuple_b = (0, 0)
-
-    if len(tuple_a) == 1:
-        tuple_a = tuple_a + (0,)
-    if len(tuple_b) == 1:
-        tuple_b = tuple_b + (0,)
-
-    for i in range(2):
-        new_list[i] = tuple_a[i] + tuple_b[i]
-    return tuple(new_list)
+    tuple_a += (0, 0)
+    tuple_b += (0, 0)
+    return tuple([tuple_a[i] + tuple_b[i] for i in range(2)])
