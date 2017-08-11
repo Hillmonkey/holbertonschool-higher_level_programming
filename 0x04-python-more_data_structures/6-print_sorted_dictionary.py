b59078aa1083f32f344b0aa7015e7f8d4c1da652
Larry Madeo <lmadeo76@gmail.com>
2017-08-11 04:23:16 +0000

    test for None first

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -3,5 +3,7 @@
 
 def print_sorted_dictionary(my_dict):
 
+    if my_dict is None or len(my_dict) == 0:
+        return
     for k in sorted(my_dict.keys()):
         print("{}: {}".format(k, my_dict[k]))
