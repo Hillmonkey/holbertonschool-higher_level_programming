43592f8101dceebcff72e311306921f300a5a858
Larry Madeo <lmadeo76@gmail.com>
2017-08-22 01:26:10 +0000

    Now handling TypeError, associated with passing
    var of type None

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -10,3 +10,5 @@ def safe_print_integer_err(value):
     except ValueError as err:
         print("Exception: {}".format(err), file=sys.stderr)
         return False
+    except TypeError as err:
+        print("Execption: {}".format(err), file=sys.stderr)
