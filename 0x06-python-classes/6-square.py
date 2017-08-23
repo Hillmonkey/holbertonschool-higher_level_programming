449483bb3b665f0bd4cc6d0c9c296f279b0ca8c5
Larry Madeo <lmadeo76@gmail.com>
2017-08-23 03:08:18 +0000

    addressing error msgs

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -28,6 +28,15 @@ class Square:
         return self.__size**2
 
     def my_print(self):
+        err_msg = "position must be a tuple of 2 positive integers"
+        if self.__position is None:
+            self.__position = (0, 0)
+        if type(self.__position) != tuple or len(self.__position) != 2:
+            self.__position = (0, 0)
+        if type(self.__position[0]) != int or type(self.__position[1]) != int:
+            self.__position = (0, 0)
+        if self.__position[0] < 0 or self.__position[1] < 0:
+            self.__position = (0, 0)
         if self.__size == 0:
             print()
             return
@@ -45,16 +54,21 @@ class Square:
         return self.__position
 
     @position.setter
-    def position(self, value):
+    def position(self, value=(0, 0)):
         err_msg = "position must be a tuple of 2 positive integers"
         if value is None:
+            self.__position = (0, 0)
             raise TypeError(err_msg)
         if type(value) != tuple or len(value) != 2:
+            self.__position = (0, 0)
             raise TypeError(err_msg)
         if type(value[0]) != int or type(value[1]) != int:
+            self.__position = (0, 0)
             raise TypeError(err_msg)
         if value[0] < 0 or value[1] < 0:
+            self.__position = (0, 0)
             raise TypeError(err_msg)
+        self.__position = value
 
 if __name__ == "__main__":
     for el in dir(Square):
