86e9c8c1155c7233a0971cc26f7b388c8acb780c
Larry Madeo <lmadeo76@gmail.com>
2017-08-23 00:44:24 +0000

    first commit

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+
+
+class Square:
+    '''Class Square
+    '''
+    pass
+
+    def __init__(self, size=0, position=(0, 0)):
+        '''init method of class Square
+        '''
+        self.__size = size
+        self.__position = position
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return self.__size**2
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+            return
+        for x in range(self.__position[1]):
+            print()
+        for i in range(self.__size):
+            for x in range(self.__position[0]):
+                print(" ", end="")
+            for j in range(self.__size):
+                print("#", end="")
+            print()
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        err_msg = "position mus be a tuple of 2 positive integers"
+        if type(value) != tuple or len(value) != 2:
+            raise TypeError(err_msg)
+        if type(value[0]) != int or type(value[1]) != int:
+            raise TypeError(err_msg)
+        if value[0] < 0 or value[1] < 0:
+            raise TypeError(err_msg)
+
+if __name__ == "__main__":
+    for el in dir(Square):
+        print(el)
