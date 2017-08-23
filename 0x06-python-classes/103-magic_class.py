3023c0f5ffe59f732b962dd2ccc30d9f5a9d5c29
Larry Madeo <lmadeo76@gmail.com>
2017-08-23 04:43:20 +0000

    added import statement

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python3
 ''' this comment takes up one line '''
+import math
 
 
 class MagicClass:
-    ''' class: MagicClass  cuz it is
-    '''
+    ''' class: MagicClass  cuz it is'''
 
     def __init__(self, radius=0):
+    ''' initialize MagicClass '''
         self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
             raise TypeError('radius must be a number')
         self.__radius = radius
 
     def area(self):
-        ''' area '''
+        ''' calculate area of circle'''
         return self.__radius**2 * math.pi
 
     def circumfrence(self):
-        '''circumfrence'''
+        '''calculate circumfrence of circle'''
         return 2 * math.pi * self.__radius
