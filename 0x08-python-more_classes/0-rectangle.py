5b6f66de07cad5ce588f3ce8ef4ca95fc4e1208f
Larry Madeo <lmadeo76@gmail.com>
2017-08-28 19:46:08 +0000

    first commit

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+'''module: 0-rectangle
+this module contains the class Rectangle ...
+'''
+
+
+class Rectangle:
+    '''class: Rectangle
+    this is an empty class, further additions in subsequent assignments
+    '''
+
+    def __init__(self):
+        '''method: __init__
+        initialize instance of class Rectangle
+        '''
+
+        pass
