297a0813a5541194698627c14f3b4aa5741f05a8
Larry Madeo <lmadeo76@gmail.com>
2017-08-29 03:13:07 +0000

    for debugging purposes

---
+++ 0x08-python-more_classes/101-modified.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+''' 
+    column first element, row second element
+'''
+def reject(board):
+    for col_A in board:
+        for col_B in board:
+            if col_A[0] == col_B[0]:
+                return True
+            if col_A[1] == col_B[1]:
+                return True
+            if col_A[1] - col_A[0] == col_B[1] - col_B[0]:
+                return True
+            if col_A[0] + col_A[1] == col_B[0] + col_B[1]:
+                return True
+    return False
+
+def accept(board):
+    if len(board) < board_size:
+        return False
+    else: # more tests def needed
+        return not(reject(board))
+
+def print_board(board):
+    print(str(board))
+
+def first(board):
+    print("DEBUG: def first()")
+    print("pre-board: {}".format(board))
+    board.append([len(board), 0])
+    print("post-board: {}".format(board))
+    return board
+
+def isdone(board):
+    if len(board) == 0:
+        return True
+    # print("DEBUG: isdone:", end='')
+    # print(board[-1][1])
+    if board[-1][1] == board_size:
+        return True
+    else:
+        return False
+
+def next_board(board):
+    board[-1][1] += 1
+    return board
+
+def bt(board):
+    # print("DEBUG: huck")
+    # print("DEBUG: " + str(board))
+    if reject(board):
+        # print("DEBUG: {}reject: {}".format(board_size, board))
+        return
+    if accept(board):
+        print_board(board)
+    new_board = first(board)
+    # print("DEBUG: new_board = {}".format(new_board))
+    while not isdone(new_board):
+        print("DEBUG: bt() while {}".format(board))
+        bt(new_board)
+        print("DEBUG: after bt() call")
+        new_board = next_board(new_board)
+
+if __name__ == '__main__':
+    board_size = 4
+    bt([])
+         
