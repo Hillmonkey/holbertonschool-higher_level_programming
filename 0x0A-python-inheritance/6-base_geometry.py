4f5756f2cc59d4acb84dba1a90f0fe41f5cb4fa2
Larry Madeo <lmadeo76@gmail.com>
2017-09-05 23:54:26 +0000

    first commit

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+''' module: 6-base_geometry
+'''
+
+
+class BaseGeometry:
+    ''' Class: BaseGeometry
+    '''
+    def area(self):
+        ''' method: area
+        raises exception
+        '''
+        raise Exception("area() is not implemented")
