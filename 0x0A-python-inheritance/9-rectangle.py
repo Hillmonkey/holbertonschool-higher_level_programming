cff5f1f56f8f76e142b9018dbc2b84170893e882
Larry Madeo <lmadeo76@gmail.com>
2017-09-07 05:25:58 +0000

    first commit

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+''' module: 9-base_geometry
+'''
+
+
+class BaseGeometry:
+    ''' Class: BaseGeometry
+    '''
+    def area(self):
+        ''' method: area
+        raises exception
+        '''
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        '''public method: integer_validator
+        name: always a string
+        value: positive integer
+        '''
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    ''' Class: BaseGeometry
+    inherits from BaseGeometry
+    '''
+    def __init__(self, width, height):
+        '''method: __init__
+        '''
+        BaseGeometry.integer_validator(self, "width", width)
+        BaseGeometry.integer_validator(self, "height", height)
+        self._width = width
+        self._height = height
+
+    def area(self):
+        '''method: area
+        '''
+        return self._width * self._height
+
+    def __str__(self):
+        '''method: __str__(self)
+        '''
+        return "[Rectangle] {}/{}".format(self._width, self._height)
+
+    def print(self):
+        '''methon: print(self)
+        '''
+        return __str__(self)
