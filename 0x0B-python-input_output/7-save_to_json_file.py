af6daaf9e0a288c7a347e42f41c4cd25f9c05f72
Larry Madeo <lmadeo76@gmail.com>
2017-09-08 01:37:16 +0000

    now letting open deal with empty string case

--- 0x0B-python-input_output/7-save_to_json_file.py
+++ 0x0B-python-input_output/7-save_to_json_file.py
@@ -9,7 +9,7 @@ def save_to_json_file(my_obj, filename):
     ''' function: save_to_json_file
     accepts Python object and sends JSON representation to file
     '''
-    if filename == "" or type(filename) is not str:
+    if type(filename) is not str:
         return
     json_string = json.dumps(my_obj)
     with open(filename, 'w') as f:
