ee43ad1a525ca7bbf1851f1c692188d5fb162d97
Larry Madeo <lmadeo76@gmail.com>
2017-09-08 01:32:00 +0000

    first commit

---
+++ 0x0B-python-input_output/7-save_to_json_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+''' module: 7-save_to_jason_file.py
+'''
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    ''' function: save_to_json_file
+    accepts Python object and sends JSON representation to file
+    '''
+    if filename == "" or type(filename) is not str:
+        return
+    json_string = json.dumps(my_obj)
+    with open(filename, 'w') as f:
+        f.write(json_string)
+        f.close
