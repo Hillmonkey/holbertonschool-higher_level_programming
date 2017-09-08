c3cbec2554efd00d26cf91831ae3a5447b78ed5d
Larry Madeo <lmadeo76@gmail.com>
2017-09-08 06:52:36 +0000

    rolled 2 statements into 1

--- 0x0B-python-input_output/7-save_to_json_file.py
+++ 0x0B-python-input_output/7-save_to_json_file.py
@@ -8,6 +8,5 @@ def save_to_json_file(my_obj, filename):
     ''' function: save_to_json_file
     accepts Python object and sends JSON representation to file
     '''
-    json_string = json.dumps(my_obj)
     with open(filename, 'w') as f:
-        f.write(json_string)
+        f.write(json.dumps(my_obj)
