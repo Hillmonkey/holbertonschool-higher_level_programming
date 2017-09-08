1dfb2a02c3a50c861e291757108aa97e1f66f067
Larry Madeo <lmadeo76@gmail.com>
2017-09-08 05:42:54 +0000

    first commit

---
+++ 0x0B-python-input_output/12-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+''' module: 12-student
+'''
+
+
+class Student:
+    '''class: student
+    '''
+
+    def __init__(self, first_name, last_name, age):
+        '''method: __init__
+        '''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        '''method: to_json
+        '''
+        full_dict = self.__dict__
+        keys = list(full_dict.keys())
+        if attrs is not None:
+            for key in keys:
+                if key in attrs:
+                    del full_dict[key]
+        return full_dict
