a1ebe10f97bea26939959764bf4947616583ffb0
Larry Madeo <lmadeo76@gmail.com>
2017-09-10 03:18:48 +0000

    first commit

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,112 @@
+#!/usr/bin/python3
+''' file/module base
+'''
+
+
+class Base:
+    '''class: Base
+    '''
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        ''' method: __init__
+        '''
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+            print("DEBUG:__init__, else case")
+
+
+class Rectangle(Base):
+    '''class: Rectangle
+    '''
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        ''' __init__
+        rectangle instantiator
+        '''
+        self.integer_GT_zero("width", width)
+        self.integer_GT_zero("height", height)
+        self.integer_GTE_zero("x", x)
+        self.integer_GTE_zero("y", y)
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+        super().__init__(id)
+
+    def integer_GT_zero(self, name, value):
+        '''public_method
+        name: always a string
+        value: positive integer
+        '''
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be > 0".format(name))
+
+    def integer_GTE_zero(self, name, value):
+        '''public_method
+        name: always a string
+        value: positive integer, or ZERO
+        '''
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value < 0:
+            raise ValueError("{} must be >= 0".format(name))
+
+    @property
+    def width(self):
+        ''' method: width getter
+        '''
+        return self.__width
+
+    @width.setter
+    def width(self, width):
+        '''method: width setter
+        '''
+        print("width.setter")
+        # integer_GT_zero("width", width)
+        self.__width = width
+
+    @property
+    def height(self):
+        ''' method: height getter
+        '''
+        return self.height
+
+    @height.setter
+    def height(self, height):
+        ''' method: height getter
+        '''
+        # integer_GT_zero("height", height)
+        self.__height = height
+
+    @property
+    def x(self):
+        ''' method: x getter
+        '''
+        return self.__x
+
+    @x.setter
+    def x(self, x):
+        ''' method: x setter
+        '''
+        # integer_GTE_zero("x", x)
+        self.__x = x
+
+    @property
+    def y(self):
+        ''' method y getter
+        '''
+        return self.__y
+
+    @y.setter
+    def y(self, y):
+        ''' method y setter
+        '''
+        # integer_GTE_zero("y", y)
+        self.__y = y
