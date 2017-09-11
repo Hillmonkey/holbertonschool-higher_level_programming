507a6749f962d8705978ef4be4fb9607efe34cc9
Larry Madeo <lmadeo76@gmail.com>
2017-09-11 23:45:31 +0000

    setter, getter & __str__ for class Square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -11,12 +11,23 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         ''' method: __init__
         '''
-        self.__width = size
-        self.__height = size
-        super().__init__(self.__width, self.__height, x, y, id)
+        # self.size = size
+        super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, size):
+        '''method: size setter
+        '''
+        super().integer_GT_zero("width", size)
+        self.width = size
+        self.height = size
 
     def __str__(self):
         ''' method: __str__ (of Square class)
         '''
-        return ("[Square] ({} {}/{} - {}/{}".format(self.id, self.x,
-                self.y, self.width, self.height))
+        return ("[Square] ({}) {}/{} - {}".format(self.id, self.x,
+                self.y, self.width))
