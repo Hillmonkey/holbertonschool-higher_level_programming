4943aef8a119ab78a82e13e13e20be3fad269742
Larry Madeo <lmadeo76@gmail.com>
2017-09-12 03:24:35 +0000

    made dicts into class:Square variables

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -7,6 +7,14 @@ from models.rectangle import Rectangle
 class Square(Rectangle):
     '''class: Square
     '''
+    key_list = ['id', 'size', 'x', 'y']
+    KV_dict = {'id': 'id', 'width': '_Rectangle__width',
+               'height': '_Rectangle__height',
+               'x': '_Rectangle__x', 'y': '_Rectangle__y'}
+    KV_2 = {'id': 'id', '_Rectangle__width': 'width',
+            '_Rectangle__height': 'height',
+            '_Rectangle__x': 'x', '_Rectangle__y': 'y'}
+
 
     def __init__(self, size, x=0, y=0, id=None):
         ''' method: __init__
@@ -35,20 +43,19 @@ class Square(Rectangle):
         '''method: update, Square class
         accepts variable length list of variables, updates attributes
         '''
-        key_list = ['id', 'size', 'x', 'y']
-        KV_dict = {'id': 'id', 'width': '_Rectangle__width',
-                   'height': '_Rectangle__height',
-                   'x': '_Rectangle__x', 'y': '_Rectangle__y'}
         for idx, el in enumerate(args):  # handle args
             if idx != 1:
-                self.__dict__[KV_dict[key_list[idx]]] = el
+                self.__dict__[Square.KV_dict[Square.key_list[idx]]] = el
             else:  # deal with size
                 self.height = el
                 self.width = el
         if len(args) == 0:  # only handle kwargs if there are no args
             for key, val in kwargs.items():
                 if key != "size":
-                    self.__dict__[KV_dict[key]] = val
+                    self.__dict__[Square.KV_dict[key]] = val
                 else:
                     self.height = val
                     self.width = val
+
+    def to_dictionary(self):
+        pass
