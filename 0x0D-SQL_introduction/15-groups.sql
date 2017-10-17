db7af63a6e65340ff8ee2e5fc4ba4a688e3022d9
Larry Madeo <lmadeo76@gmail.com>
2017-10-16 23:02:50 -0700

    fixed error: referencing alias within scope

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,8 +1,8 @@
 -- display scores, grouped by count, in descending order of 
-SELECT score, COUNT(score) AS "number"
+SELECT score, COUNT(score) AS number
 FROM
 	second_table
 GROUP BY
 	score
 ORDER BY
-	count(number) DESC;
+	COUNT(score) DESC;
