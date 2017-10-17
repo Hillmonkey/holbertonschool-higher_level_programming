f2c105b0c54a60b23860e99b5d9e738696d42adb
Larry Madeo <lmadeo76@gmail.com>
2017-10-16 19:54:41 -0700

    remove extraneous semi-colon

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -2,7 +2,7 @@
 CREATE TABLE IF NOT EXISTS second_table
 	(id INT, name VARCHAR(256), score INT);
 INSERT INTO second_table
-	(id, name, score);
+	(id, name, score)
 	VALUES
     (id = 1, name = 'John', score = 10),
     (id = 2, name = 'Alex', score = 3),
