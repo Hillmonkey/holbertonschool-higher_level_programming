d1b3182bfa3abbc8a378d7faec05e92ac147ca7f
Larry Madeo <lmadeo76@gmail.com>
2017-10-17 19:41:46 -0700

    defensively re-create state table, also
    change reference to states(id) instead of states(state_id)

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,7 +1,11 @@
 -- create a database and within it, a table of states
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(
+	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	name VARCHAR(256) NOT NULL
+);
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
 	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	FOREIGN KEY (state_id) REFERENCES states(state_id),
+	FOREIGN KEY (state_id) REFERENCES states(id),
 	name VARCHAR(256) NOT NULL
 );
