473312bb6686d00f1796d15090e75052e073136a
Larry Madeo <lmadeo76@gmail.com>
2017-10-17 19:49:52 -0700

    separate line to explicitly create state_id in
    cities

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -6,6 +6,7 @@ CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(
 );
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
 	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	state_id INT,
 	FOREIGN KEY (state_id) REFERENCES states(id),
 	name VARCHAR(256) NOT NULL
 );
