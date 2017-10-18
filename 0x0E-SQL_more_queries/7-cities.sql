c4971a0d5d516fd29cdd09a69e1aacf4680199dc
Larry Madeo <lmadeo76@gmail.com>
2017-10-17 19:53:31 -0700

    added NOT NULL constraint to state_id

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(
 );
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
 	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	state_id INT,
+	state_id INT NOT NULL,
 	FOREIGN KEY (state_id) REFERENCES states(id),
 	name VARCHAR(256) NOT NULL
 );
