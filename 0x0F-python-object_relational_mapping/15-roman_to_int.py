1d6807ff2de23a67cca4c6c90460138cc19eb9da
Larry Madeo <lmadeo76@gmail.com>
2017-11-11 08:02:29 +0000

    pep8 fix

--- 0x0F-python-object_relational_mapping/15-roman_to_int.py
+++ 0x0F-python-object_relational_mapping/15-roman_to_int.py
@@ -2,19 +2,18 @@
 '''module: 15-roman_to_int
 '''
 
+
 def roman_to_int(roman_string):
-	'''function: roman_to_int
-	accepts: string representation of roman number (all uppercase) (1 - 3,999)
-	returns: an int
-	'''
-	roms = {"M":0 , "D":1, "C":2, "L":3, "X":4, "V":5, "I":6}
-	ints = [1000, 500, 100, 50, 10, 5, 1]
+    '''function: roman_to_int
+    accepts: string representation of roman number (all uppercase) (1 - 3,999)
+    returns: an int
+    '''
+    roms = {"M": 0, "D": 1, "C": 2, "L": 3, "X": 4, "V": 5, "I": 6}
+    ints = [1000, 500, 100, 50, 10, 5, 1]
 
-	total = 0
+    total = 0
 
-	for roman_char in roman_string:
-		total += ints[roms[roman_char]]
+    for roman_char in roman_string:
+        total += ints[roms[roman_char]]
 
-	return total
-		
-
+    return total
