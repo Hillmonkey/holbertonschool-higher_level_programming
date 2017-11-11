43459f4ca5fa262f430ffb6e49aab26e33410739
Larry Madeo <lmadeo76@gmail.com>
2017-11-11 02:21:26 +0000

    first commit

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Module: 8-model_state_fetch_first
+Links class to table in database, then fetches filtered query
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine, select)
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
+
+    connection = engine.connect()
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+    # results = session.query(State).first()
+    results = session.query(State).filter(State.name.op('regexp')('a'))
+
+    if results is None:
+        print("Nothing")
+    else:
+        for result in results:
+            print("{}: {}".format(result.id, result.name))
