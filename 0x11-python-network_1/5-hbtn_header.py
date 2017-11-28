bf16cd1c29e9186c8fc1063c227d4a6cb42e6e42
Larry Madeo <lmadeo76@gmail.com>
2017-11-27 19:35:16 -0800

    first commit :

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+import requests
+import sys
+
+'''module: 5-hbtn_header
+accepts: URL from the command line
+output: value of X-Request-Id
+'''
+
+
+if __name__ == '__main__':
+    r = requests.get(sys.argv[1])
+    print(r.headers["X-Request-Id"])
