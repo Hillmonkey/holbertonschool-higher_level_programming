86320346a39374f28e49e4a5a4786e4bfe5a5bf6
Larry Madeo <lmadeo76@gmail.com>
2017-11-28 23:45:12 +0000

    pre shellcheck

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
-# this script DOES WHAT?!?!
+# this prints Content-Size of URL resource
+# curl -I $1 | grep Content-Size
+curl -sI 0.0.0.0:5000 | grep "^Content-Length" | cut -d' ' --fields=2
