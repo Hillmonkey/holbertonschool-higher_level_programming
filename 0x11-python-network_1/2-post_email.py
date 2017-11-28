677d2337954a8fa18a0449fbaee6783a60b69cbb
Larry Madeo <lmadeo76@gmail.com>
2017-11-27 18:54:28 -0800

    first commit

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+import sys
+from urllib.request import Request, urlopen
+from urllib.parse import urlencode
+
+'''module: 2-post_email.py
+accepts: 2 command line args - (1) URL, (2) email
+         I think email is supposed to be a valid email address
+output: body of response
+'''
+
+
+if __name__ == "__main__":
+
+    url = sys.argv[1]
+    data = urlencode({"email": sys.argv[2]}).encode('utf-8')
+    try:
+        req = Request(url, data)
+        with urlopen(req) as response:
+            response.read().decode('utf-8')
+    except URLError:
+        pass
