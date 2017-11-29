f2ff6795076cd646911ffd1211ab52213b2c0aed
Larry Madeo <lmadeo76@gmail.com>
2017-11-29 01:22:55 +0000

    syntax and shellcheck fixes

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # this accepts URL and prints body of HTTP response
 STATUSCODE=$(curl -s -o /dev/null -w "%{http_code}" "$1")
-if [$STATUSCODE = 200] then
+if [ "$STATUSCODE" = 200 ]; then
     curl -s "$1"
 fi
