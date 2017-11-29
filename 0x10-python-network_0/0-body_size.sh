0df42a280ba94588a8e9edb0c6200bf16e43e24f
Larry Madeo <lmadeo76@gmail.com>
2017-11-29 00:52:03 +0000

    accepts command line arg and passes shellcheck

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
-# this prints Content-Size of URL resource
-# curl -I $1 | grep Content-Size
-curl -sI 0.0.0.0:5000 | grep "^Content-Length" | cut -d' ' --fields=2
+# this prints Content-Size of passed inURL resource
+curl -sI "$1" | grep "^Content-Length" | cut -d' ' --fields=2
