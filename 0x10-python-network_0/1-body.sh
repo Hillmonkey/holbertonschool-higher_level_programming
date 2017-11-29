7886e3f7380d88102c1b44355c32fad2519aa9d4
Larry Madeo <lmadeo76@gmail.com>
2017-11-29 01:58:08 +0000

    using -L to follow to final destination

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
 # this accepts URL and prints body of HTTP response
-STATUSCODE=$(curl -s -o /dev/null -w "%{http_code}" "$1")
-if [ "$STATUSCODE" = 200 ]; then
-    curl -s "$1"
-fi
+curl -sL "$1"
