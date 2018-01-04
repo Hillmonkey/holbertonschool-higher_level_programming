d1b9fe8b56e435d7f6eb7147c8ac206a53a28be1
Larry Madeo <lmadeo76@gmail.com>
2018-01-03 20:10:06 -0800

    remove commented out code

--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -3,13 +3,3 @@
 exports.add = function (a, b) {
   return (a + b);
 };
-
-/*
-let a = Math.floor(process.argv[2]);
-let b = Math.floor(process.argv[3]);
-if (isNaN(a) || isNaN(b)) {
-  console.log('NaN');
-} else {
-  console.log(add(a, b));
-}
-*/
