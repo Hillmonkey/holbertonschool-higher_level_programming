202afb58a84be20c0f4fc30826b7a7b94a8dae4f
Larry Madeo <lmadeo76@gmail.com>
2018-01-03 19:01:17 -0800

    first commit

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+'use strict';
+
+function add (a, b) {
+  return (a + b);
+}
+
+let a = Math.floor(process.argv[2]);
+let b = Math.floor(process.argv[3]);
+if (isNaN(a) || isNaN(b)) {
+  console.log('NaN');
+} else {
+  console.log(add(a, b));
+}
