694d2f5571f6cf715141d6e1bbc294c322c13239
Larry Madeo <lmadeo76@gmail.com>
2018-01-09 14:36:23 -0800

    first commit

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+'use strict';
+const Rectangle = class Rectangle {
+  constructor (width, height) {
+    this.width = width;
+    this.height = height;
+  }
+};
+module.exports = Rectangle;
