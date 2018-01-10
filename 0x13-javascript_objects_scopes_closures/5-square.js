269a4e38b9aadd277fa19534a2a8ef036c292e61
Larry Madeo <lmadeo76@gmail.com>
2018-01-09 16:06:33 -0800

    importing Rectangle now

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,32 +1,6 @@
 #!/usr/bin/node
 'use strict';
-const Rectangle = class Rectangle {
-  constructor (width, height) {
-    if ((width > 0) && (height > 0)) {
-      this.width = width;
-      this.height = height;
-    }
-  }
-  print () {
-    let line = '';
-    for (let i = 0; i < this.width; i++) {
-      line += 'X';
-    }
-    for (let j = 0; j < this.height; j++) {
-      console.log(line);
-    }
-  }
-  double () {
-    this.width *= 2;
-    this.height *= 2;
-  }
-  rotate () {
-    let temp = this.width;
-    this.width = this.height;
-    this.height = temp;
-  }
-};
-
+const Rectangle = require('./4-rectangle');
 const Square = class Square extends Rectangle {
   constructor (size) {
     super(size, size);
