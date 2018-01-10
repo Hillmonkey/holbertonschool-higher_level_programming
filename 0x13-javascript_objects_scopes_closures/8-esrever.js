b549acce40dc4227628d3566446e5cca2d8cf742
Larry Madeo <lmadeo76@gmail.com>
2018-01-09 18:26:25 -0800

    var to let

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -3,7 +3,7 @@
 
 exports.esrever = function (list) {
   let ret = [];
-  for (var i = list.length - 1; i >= 0; i--) {
+  for (let i = list.length - 1; i >= 0; i--) {
     ret.push(list[i]);
   }
   return ret;
