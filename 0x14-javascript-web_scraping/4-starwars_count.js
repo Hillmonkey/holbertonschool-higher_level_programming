dc3aa125d6b971b8ec25459631d586bcc7081f76
Larry Madeo <lmadeo76@gmail.com>
2018-01-10 21:16:47 -0800

    use endsWith() method

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,19 +1,20 @@
 #!/usr/bin/node
+// invoke this script as follows:
+// ./4-starwars_count.js http://swapi.co/api/films
+
 'use strict';
 const request = require('request');
 const url = process.argv[2];
-const wedgeAntilles = 'https://swapi.co/api/people/18/';
+const wedgeAntilles = '/18/';
 request(url, function (error, response, body) {
   if (error) {}
   let count = JSON.parse(response.body).count;
   let starWarsJSON = JSON.parse(response.body).results;
   let movieCount = 0;
   for (let i = 0; i < count; i++) {
-    // console.log(starWarsJSON[i].title);
     let characters = starWarsJSON[i].characters;
     for (let character of characters) {
-      // console.log(character, typeof(character));
-      if (character === wedgeAntilles) {
+      if (character.endsWith(wedgeAntilles)) {
         movieCount++;
       }
     }
