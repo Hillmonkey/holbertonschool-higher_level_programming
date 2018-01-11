468b4eb2e3f9339ee5fd003a503670304667cb11
Larry Madeo <lmadeo76@gmail.com>
2018-01-10 16:19:55 -0800

    from throw err to console.log(err)

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -5,6 +5,9 @@ const fname = process.argv[2];
 
 // this gives me a buffer object without utf8 specified ...
 fs.readFile(fname, 'utf8', function (err, data) {
-  if (err) throw err;
-  console.log(data);
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
 });
