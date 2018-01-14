5147faae43762036243f8c7501dc21d114705f9a
Larry Madeo <lmadeo76@gmail.com>
2018-01-14 12:49:04 -0800

    first commit

---
+++ 0x15-javascript-web_jquery/102-script.js
@@ -0,0 +1,9 @@
+$.when($.ready).then(function () {
+  $('INPUT#btn_search').click(function () {
+    let city = $('INPUT#city_search').val();
+    $.getJSON('https://query.yahooapis.com/v1/public/yql?q=select%20wind%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22:' + city + '%22)&format=json', function (data) {
+      let windSpeed = data.query.results.channel.wind.speed;
+      $('DIV#wind_speed').text(windSpeed);
+    });
+  });
+});
